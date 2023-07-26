48d39878221dc2d62f44b2f0c7940ad60d47d40f
ssandlin12 <ssandlin12@gmail.com>
2023-07-26 16:01:54 +0200

    finalized favorites

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -61,7 +61,7 @@ function FavoritesPage() {
                 >
                   <div className="flex gap-x-4 pr-6 sm:w-1/2 sm:flex-none">
                     <img
-                      className="h-12 w-12 flex-none rounded-full bg-gray-50 my-4"
+                      className="h-16 w-16 flex-none rounded-full bg-gray-50 my-2 object-cover"
                       src={profile.image}
                     ></img>
                     <div className="min-w-0 flex-auto my-4">
