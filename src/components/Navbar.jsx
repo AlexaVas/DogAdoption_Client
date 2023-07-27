c975824dfdf5bb3d86528b26c0371edc5679f769
AlexaVas <alexandra.vasinova@gmail.com>
2023-07-27 12:28:42 +0200

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,10 +28,9 @@ function Navbar() {
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex">
             <Link to="/">
-              <a className="-m-1.5 p-1.5">
-                <span className="sr-only">Barkly</span>
+
                 <img className="h-8 w-auto" src="/src/assets/fullLogo.svg" alt="" />
-              </a>
+
             </Link>
           </div>
 
