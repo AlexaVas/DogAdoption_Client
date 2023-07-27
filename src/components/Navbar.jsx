e67a4f526edd480d9f2c8ccf660d2c33dbb9356f
ssandlin12 <ssandlin12@gmail.com>
2023-07-27 16:44:13 +0200

    reverted changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Link } from "react-router-dom";
-import { Fragment, useState } from "react";
+import { Fragment, useState } from 'react';
 import fullLogo from "/src/assets/fullLogo.svg";
-import { Dialog, Popover, Transition } from "@headlessui/react";
+import { Dialog, Popover, Transition } from '@headlessui/react'
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -12,27 +12,13 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
   XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 const loginTypes = [
-  {
-    name: "Individual",
-    description: `I'm looking for a dog`,
-    href: "/login",
-    icon: UserIcon,
-  },
-  {
-    name: "Shelter",
-    description: "Manage list of dogs",
-    href: "/shelter",
-    icon: HomeIcon,
-  },
-];
+  { name: 'Individual', description: `I'm looking for a dog`, href: '/login', icon: UserIcon },
+  { name: 'Shelter', description: 'Manage list of dogs', href: '/shelter', icon: HomeIcon },
+]
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
@@ -42,19 +28,20 @@ function Navbar() {
       <header className="bg-white">
         <nav
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
+          aria-label="Global">
           <div className="flex">
             <Link to="/">
-              <span className="sr-only">Barkly</span>
-              <img className="h-8 w-auto" src={fullLogo} alt="" />
+
+                <span className="sr-only">Barkly</span>
+                <img className="h-8 w-auto" src={fullLogo} alt="" />
+
             </Link>
           </div>
 
           {!isLoggedIn && (
             <>
               <Popover className="relative">
-                <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold   text-gray-900">
+                <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
                   Log in
                   <ChevronDownIcon
                     className="h-5 w-5 flex-none text-gray-400"
@@ -69,15 +56,13 @@ function Navbar() {
                   enterTo="opacity-100 translate-y-0"
                   leave="transition ease-in duration-150"
                   leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
+                  leaveTo="opacity-0 translate-y-1">
                   <Popover.Panel className="absolute -right-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                     <div className="p-4">
                       {loginTypes.map((item) => (
                         <div
                           key={item.name}
-                          className="group relative flex gap-x-6 rounded-lg p-4 text-sm   hover:bg-gray-50"
-                        >
+                          className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
                           <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                             <item.icon
                               className="h-6 w-6 text-gray-600 group-hover:text-blue-600"
@@ -85,10 +70,9 @@ function Navbar() {
                             />
                           </div>
                           <div className="flex-auto">
-                            <Link
-                              to={item.href}
-                              className="block font-semibold text-gray-900"
-                            >
+
+                              <Link to={item.href}
+                              className="block font-semibold text-gray-900">
                               {item.name}
                               <span className="absolute inset-0" />
                             </Link>
@@ -112,10 +96,7 @@ function Navbar() {
   if (user.userType === "shelter") {
     return (
       <header className="bg-white">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex">
             <Link to="/">
               <a className="-m-1.5 p-1.5">
@@ -125,65 +106,55 @@ function Navbar() {
             </Link>
           </div>
 
-          {/*    UPDATE     */}
-          {isLoggedIn && (
-            <>
-              <div className="flex  gap-x-8">
-                <Link to="/shelter/profile">
-                  {" "}
-                  <button className="text-sm font-semibold   text-gray-900">
-                    Our Listings
-                  </button>
-                </Link>
+        {/*    UPDATE     */}
+        {isLoggedIn && (
+          <>
+          <div className="flex  gap-x-8">
 
-                <Link to="/shelter/listing">
-                  <button className="text-sm font-semibold   text-gray-900">
-                    Add Listing
-                  </button>
-                </Link>
-              </div>
 
-              <div className="flex items-center">
-                <HomeIcon className="h-5 w-5 text-gray-600 mr-4" />
-                <p className="mr-4 text-gray-800 text-sm font-medium">
-                  {user && user.name}
-                </p>
-                <button
-                  className="text-sm font-semibold   text-gray-900"
-                  onClick={logOutUser}
-                >
-                  Logout
-                </button>
-              </div>
-            </>
-          )}
+<Link to="/shelter/profile"> <button className="text-sm font-semibold leading-6 text-gray-900">
+  Our Listings
+  </button>
+</Link>
 
-          {!isLoggedIn && (
-            <>
-              <Link to="/signup">
-                {" "}
-                <button>Sign Up</button>{" "}
-              </Link>
-              <Link to="/login">
-                {" "}
-                <button>Login</button>{" "}
-              </Link>
-              <Link to="/shelter">
-                {" "}
-                <button>Shelter</button>{" "}
-              </Link>
-            </>
-          )}
-        </nav>
+<Link to="/shelter/listing"><button className="text-sm font-semibold leading-6 text-gray-900">
+  Add Listing</button>
+</Link>
+
+
+</div>
+
+<div className="flex items-center">
+<HomeIcon className="h-5 w-5 text-gray-600 mr-4"  />
+<p className="mr-4 text-gray-800 text-sm font-medium">{user && user.name}</p>
+<button className="text-sm font-semibold leading-6 text-gray-900" onClick={logOutUser}>Logout</button>
+</div>
+          </>
+        )}
+
+        {!isLoggedIn && (
+          <>
+            <Link to="/signup">
+              {" "}
+              <button>Sign Up</button>{" "}
+            </Link>
+            <Link to="/login">
+              {" "}
+              <button>Login</button>{" "}
+            </Link>
+            <Link to="/shelter">
+              {" "}
+              <button>Shelter</button>{" "}
+            </Link>
+          </>
+        )}
+      </nav>
       </header>
     );
   } else {
     return (
-      <header className="bg-white">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
+         <header className="bg-white">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex">
             <Link to="/">
               <a className="-m-1.5 p-1.5">
@@ -193,32 +164,26 @@ function Navbar() {
             </Link>
           </div>
 
-          {isLoggedIn && (
-            <>
-              <Link
-                to="/user/favorites"
-                className="text-sm font-semibold   text-gray-900"
-              >
-                <button>Favorites</button>
-              </Link>
+        {isLoggedIn && (
+          <>
+            <Link to="/user/favorites" className="text-sm font-semibold leading-6 text-gray-900">
+              <button>Favorites</button>
+            </Link>
 
-              <div className="flex items-center">
-                <UserIcon className="h-5 w-5 text-gray-600 mr-4" />
-                <p className="mr-4 text-gray-800 text-sm font-medium">
-                  {user && user.name}
-                </p>
-                <button
-                  className="text-sm font-semibold   text-gray-900"
-                  onClick={logOutUser}
-                >
-                  Logout
-                </button>
-              </div>
-            </>
-          )}
-        </nav>
+
+            <div className="flex items-center">
+            <UserIcon className="h-5 w-5 text-gray-600 mr-4"  />
+            <p className="mr-4 text-gray-800 text-sm font-medium">{user && user.name}</p>
+<button className="text-sm font-semibold leading-6 text-gray-900" onClick={logOutUser}>Logout</button>
+</div>
+
+          </>
+        )}
+
+      </nav>
       </header>
     );
+
   }
 }
 
